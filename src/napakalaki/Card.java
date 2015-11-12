15cf24935863c816e7930a602bec45b212e2d9f5
Carlos <pepitogrillo148@hotmail.com>
2015-11-12 12:30:06 +0100

    Comentarios de las variables y los metodos usados

--- src/napakalaki/Card.java
+++ src/napakalaki/Card.java
@@ -1,11 +1,13 @@
-//  PDOO GRUPO A
-//  JESUS ANGEL GONZALEZ NOVEZ   76440070F
-//  CARLOS DE LA TORRE			 75145459C
 package napakalaki;
 
 /**
- *
- * @author root
+ * Interface que sirve para agregar metodos basicos
+ * a las diferentes clases del juego, y de esta forma
+ * que haya una uniformidad en los diferentes métodos
+ * de las clases
+ * @authors:
+ * Carlos de la Torre 75145459C
+ * Farouk
  */
 public interface Card {
     public String getName();
